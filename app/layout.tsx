de92593e67dbd1d53e90525570b3e2bb06b78c61
Victor <vi7org@hotmail.com>
2024-12-21 12:38:49 -0500

    initial setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import {Notifications} from "@mantine/notifications";
+import {ColorSchemeScript, mantineHtmlProps, MantineProvider} from "@mantine/core";
+import {theme} from "@/theme";
+import '@mantine/core/styles.css';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,10 +16,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang="en" {...mantineHtmlProps}>
+    <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
+      <ColorSchemeScript defaultColorScheme={"dark"}/>
+    </head>
+
+    <body>
+    <MantineProvider theme={theme}>
+      <Notifications/>
+      {children}
+    </MantineProvider>
+    </body>
     </html>
   );
 }
