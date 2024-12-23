8d7e7ba4d0a53dc0279a5aa92f7fa2f5b1b41774
Victor <vi7org@hotmail.com>
2024-12-23 07:59:22 -0500

    add base colors to postcss config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,6 +3,7 @@
         'postcss-preset-mantine': {},
         'postcss-simple-vars': {
             variables: {
+                // Breakpoints
                 'mantine-breakpoint-xss': '22.5em',
                 'mantine-breakpoint-xs': '36em',
                 'mantine-breakpoint-sm': '48em',
@@ -10,6 +11,11 @@
                 'mantine-breakpoint-lg': '75em',
                 'mantine-breakpoint-xl': '88em',
                 'mantine-breakpoint-xxl': '120em',
+
+                // Colors
+                'color-primary': '#FF6500',
+                'color-secondary': '#1E3E62',
+                'color-tertiary': '#0B192C',
             },
         },
     },
