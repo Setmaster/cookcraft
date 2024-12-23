bfee139bbdfb2ecd14f653151cc89b9d35e39710
Victor <vi7org@hotmail.com>
2024-12-23 12:42:05 -0500

    add dev dashboard page

---
+++ app/devdashboard/page.tsx
@@ -0,0 +1,21 @@
+﻿'use client';
+import { Button } from '@mantine/core';
+import { useRouter } from 'next/navigation';
+
+export default function DevDashboard() {
+    const router = useRouter();
+
+    const handleButtonClick = () => {
+        // Navigate to a non-existent page to test the 404 page
+        router.push('/non-existent-page');
+    };
+
+    return (
+        <main>
+            <h1>This is the dev dashboard page</h1>
+            <Button onClick={handleButtonClick}>
+                404 Error Test
+            </Button>
+        </main>
+    );
+}
