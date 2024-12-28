4d3fcc4ba4ce0827dd2951babcfa75046b738fae
edgmsh <edgmsh@gmail.com>
2024-12-28 13:39:58 -0500

    correct naming to a helper function

--- utils/indexHelpers.ts
+++ utils/indexHelpers.ts
@@ -1,3 +1,3 @@
-export function validateString(newValue: string, supportingValue: string): string {
-  return newValue === null || newValue === undefined || newValue === "" ? supportingValue : newValue;
+export function validateString(newValue: string, alternativeValue: string): string {
+  return newValue === null || newValue === undefined || newValue === "" ? alternativeValue : newValue;
 }
