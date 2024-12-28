37cd1c0deb3725f822fed61ebc74e29900feba1a
Victor <vi7org@hotmail.com>
2024-12-27 21:21:05 -0500

    create recipe card

---
+++ components/RecipeCard/RecipeImage.tsx
@@ -0,0 +1,35 @@
+﻿'use client';
+
+import Image from "next/image";
+import classes from './RecipeImage.module.css';
+import {useState} from "react";
+import {LoadingOverlay} from "@mantine/core";
+
+// type RecipeImageProps = {
+//    
+// };
+
+export default function RecipeImage() {
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    return (
+        <>
+            <LoadingOverlay
+                visible={isLoading}
+                zIndex={1000}
+                overlayProps={{ radius: "sm", blur: 25 }}
+                loaderProps={{ color: 'red', type: 'bars' }}
+            />
+            <Image
+                className={classes.saleImage}
+                src={"https://images.unsplash.com/photo-1562923690-e274ba919781?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+                alt={``}
+                fill
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                style={{ objectFit: 'cover', objectPosition: 'center' }}
+                onLoad={() => setIsLoading(false)}
+            />
+        </>
+    );
+}
---
+++ components/Recipes/Listings.tsx
@@ -0,0 +1,22 @@
+﻿import {Container, SimpleGrid} from "@mantine/core";
+import classes from './Listings.module.css';
+import RecipeCard from "@/components/RecipeCard/RecipeCard";
+
+export default function Listings(){
+    return (
+        <>
+            <Container fluid className={classes.listingsContainer}>
+                <SimpleGrid cols={{base: 1, sm: 2, md: 3, xl: 4}}>
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                    <RecipeCard />
+                </SimpleGrid>
+            </Container>
+        </>
+    )
+}
