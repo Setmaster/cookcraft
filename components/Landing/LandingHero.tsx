a8d31aae12b98ae7f9f96302dd6353f53113b0d6
Andrew <andrewlang.p@gmail.com>
2025-01-06 20:51:25 -0500

    add modal for landing page create recipe button

--- components/Landing/LandingHero.tsx
+++ components/Landing/LandingHero.tsx
@@ -1,21 +1,54 @@
-﻿import { Button, Container, Overlay, Text, Title } from '@mantine/core';
-import classes from './LandingHero.module.css';
+﻿import { Button, Container, Modal, Overlay, Text, Title } from "@mantine/core";
+import classes from "./LandingHero.module.css";
+import { authClient } from "@/lib/auth-client";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { Login } from "@/components/Authentication/Login";
 
 export function LandingHero() {
-    return (
-        <div className={classes.hero}>
-            <Overlay
-                gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
-                opacity={1}
-                zIndex={0}
-            />
-            <Container className={classes.container} size="md">
-                <Title className={classes.title}>Got some ingredients sitting around? Ask our AI-powered recipe maker and start making your meal now! </Title>
+  const { data: session, isPending, error } = authClient.useSession();
+  const [loginModalOpened, setLoginModalOpened] = useState(false);
+  const router = useRouter();
 
-                <Button variant="gradient" size="xl" radius="xl" className={classes.control}>
-                    Create a recipe now
-                </Button>
-            </Container>
-        </div>
-    );
+  const handleButtonClick = () => {
+    if (session) {
+      router.push("/recipes");
+    } else {
+      setLoginModalOpened(true);
+    }
+  };
+
+  return (
+    <div className={classes.hero}>
+      <Overlay
+        gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
+        opacity={1}
+        zIndex={0}
+      />
+      <Container className={classes.container} size="md">
+        <Title className={classes.title}>
+          Got some ingredients sitting around? Ask our AI-powered recipe maker
+          and start making your meal now!{" "}
+        </Title>
+
+        <Button
+          variant="gradient"
+          size="xl"
+          radius="xl"
+          className={classes.control}
+          onClick={handleButtonClick}
+        >
+          Create a recipe now
+        </Button>
+
+        <Modal
+          opened={loginModalOpened}
+          onClose={() => setLoginModalOpened(false)}
+          title="Login to Your Account"
+        >
+          <Login />
+        </Modal>
+      </Container>
+    </div>
+  );
 }
