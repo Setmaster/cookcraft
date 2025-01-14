1fa7d8e9e9c11bd728a103c69df81fa8a8debcf1
Victor <vi7org@hotmail.com>
2025-01-14 14:35:21 -0500

    fix misalignment on landing page's description

--- components/Landing/FeatureCards.tsx
+++ components/Landing/FeatureCards.tsx
@@ -5,6 +5,7 @@ import {
     Container,
     Group,
     SimpleGrid,
+    Stack,
     Text,
     Title,
     useMantineTheme,
@@ -15,7 +16,7 @@ const mockdata = [
     {
         title: 'Save time',
         description:
-            "Save your precious time from trying to come up with recipes. There are thousands of recipes out there, you just need the right ingredients.",
+            'Save your precious time from trying to come up with recipes. There are thousands of recipes out there, you just need the right ingredients.',
         icon: IconClockHour12,
     },
     {
@@ -27,7 +28,7 @@ const mockdata = [
     {
         title: 'Reduce food waste',
         description:
-            "Remember those potatos sitting in the pantry? Or the leftover ground beef nearing its expiry date? Use those in a recipe instead of throwing them out.",
+            'Remember those potatoes sitting in the pantry? Or the leftover ground beef nearing its expiry date? Use those in a recipe instead of throwing them out.',
         icon: IconTrash,
     },
 ];
@@ -36,13 +37,15 @@ export function FeaturesCards() {
     const theme = useMantineTheme();
     const features = mockdata.map((feature) => (
         <Card key={feature.title} shadow="md" radius="md" className={classes.card} padding="xl">
-            <feature.icon size={50} stroke={2} color={theme.colors.blue[6]} />
-            <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
-                {feature.title}
-            </Text>
-            <Text fz="sm" c="dimmed" mt="sm">
-                {feature.description}
-            </Text>
+            <Stack align="center">
+                <feature.icon size={50} stroke={2} color={theme.colors.blue[6]} />
+                <Text fz="lg" fw={500} className={classes.cardTitle} mt="md">
+                    {feature.title}
+                </Text>
+                <Text fz="sm" c="dimmed" mt="sm" ta="center">
+                    {feature.description}
+                </Text>
+            </Stack>
         </Card>
     ));
 
@@ -55,11 +58,12 @@ export function FeaturesCards() {
             </Group>
 
             <Title order={2} className={classes.title} ta="center" mt="sm">
-                Generate recpies with any ingredients in a single prompt
+                Generate recipes with any ingredients in a single prompt
             </Title>
 
             <Text c="dimmed" className={classes.description} ta="center" mt="md">
-                Type your list of ingredients and pass it to Cookcraft. It will generate a list of recipes that you can make with those ingredients.
+                Type your list of ingredients and pass it to Cookcraft. It will generate a list of recipes
+                that you can make with those ingredients.
             </Text>
 
             <SimpleGrid cols={{ base: 1, md: 3 }} spacing="xl" mt={50}>
